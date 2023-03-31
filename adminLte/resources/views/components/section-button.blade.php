b8d89bae680518fde26150c89017bb44337bb485
Mónica R <a20monicarr@gmail.com>
2023-03-31 23:46:24 +0200

    add margin button

--- adminLte/resources/views/components/section-button.blade.php
+++ adminLte/resources/views/components/section-button.blade.php
@@ -1,5 +1,5 @@
 @vite(['resources/js/app.js','resources/css/app.css'])
 
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 mt-5 bg-naranja border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-melocoton active:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-10 py-2 mt-5 bg-naranja border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-melocoton active:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
