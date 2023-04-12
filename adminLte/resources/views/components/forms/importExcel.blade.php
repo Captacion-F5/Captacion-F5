b61d1dd03c2abc30c80905f8bf858465cc129ebc
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-04-12 08:24:20 +0200

    add importExcel form

---
+++ adminLte/resources/views/components/forms/importExcel.blade.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Importar Postulantes</div>
+                <div class="card-body">
+                    @if (isset($errors) && $errors->any())
+                    <div class="alert alert-danger" role="alert">
+                        @foreach ($errors->all() as $error)
+                        @endforeach
+                    </div>
+                    @endif
+                    <form action="{{route(postulado.store)}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <input type="file" name="import_file"/>
+                    <button class="btn btn-primary" type="submit">Importar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
