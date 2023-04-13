2e5170a7e2e13657b2580e68825083f38296c501
TaniaPandal <psiqonautas@gmail.com>
2023-04-13 11:10:43 +0200

    saving

--- adminLte/app/Http/Controllers/EventPostuladoController.php
+++ adminLte/app/Http/Controllers/EventPostuladoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\EventPostulado;
 use Illuminate\Http\Request;
 
-class EventPostuladoController extends Controller
+
 {
     /**
      * Display a listing of the resource.
