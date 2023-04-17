f0f330f63d0cbc89040810d06fe51f1f796a83d8
anagararna <anagararna@gmail.com>
2023-04-17 10:29:17 +0200

    add donut

--- adminLte/resources/views/components/molecules/donut-genero-fem-norte.blade.php
+++ adminLte/resources/views/components/molecules/donut-genero-fem-norte.blade.php
@@ -2,7 +2,7 @@
 
 <div class="shadow-lg rounded-lg overflow-hidden w-full md:w-80 mx-2 md:mx-4 mt-4 md:my-2">
   <div class="w-full h-80 md:h-full relative p-0 m-0">
-    <div class="py-3 px-5 bg-gray-50 w-auto h-auto relative top-0 left-0">Participación por género</div>
+    <div class="py-3 px-5 bg-gray-50 w-auto h-auto relative top-0 left-0">Participación FemNorte</div>
     <canvas class="p-10 w-full h-full absolute top-0 left-0" id="chartDoughnutGenero"></canvas>
   </div>
 </div>
