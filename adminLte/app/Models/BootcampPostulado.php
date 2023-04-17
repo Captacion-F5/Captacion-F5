05a3da5e60e3fb86b2a52eab393fc8c9050335ae
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-04-17 12:46:02 +0200

    update name to class BootcampPostulado

--- adminLte/app/Models/BootcampPostulado.php
+++ adminLte/app/Models/BootcampPostulado.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PostuladoAsistenciaEvento extends Model
+class BootcampPostulado extends Model
 {
     use HasFactory;
     protected $table = 'bootcamp_postulado';
