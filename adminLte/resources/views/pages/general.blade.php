838e70758dd61a9f6421f57d0ca574f499d553d5
Mónica R <a20monicarr@gmail.com>
2023-04-22 16:32:37 +0200

    merge dev

--- adminLte/resources/views/pages/general.blade.php
+++ adminLte/resources/views/pages/general.blade.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\BootcampController;
 
 
 @section('content')
-<h2 class="text-naranja text-lg mt-2">Bienvenida a tu panel de administradora</h2>
+    <div class="ml-3">
+        @include('/components/atoms/panel')
+    </div>
 <div class="flex m-6">
     <x-general-header-buttons :bootcamp="$bootcamp"></x-general-header-buttons>
     @foreach ($event as $evento)
@@ -23,37 +25,7 @@ use App\Http\Controllers\BootcampController;
         @include('/components/grafics/exer')
         @include('/components/grafics/even')
     </div>
-        <div class="flex justify-between">
-            <div class="m-10">
-                <a href="{{ url('/general') }}">
-                    <x-section-button class="m-auto">
-                        {{ __('General') }}
-                    </x-section-button>
-                </a>
-                <a href="{{ url('/exercises') }}">
-                    <x-section-button class="">
-                        {{ __('Ejercicios') }}
-                    </x-section-button>
-                </a>
-                <x-section-button class="">
-                    {{ __('RIC') }}
-                </x-section-button>
-                <x-section-button class="">
-                    {{ __('JPA') }}
-                </x-section-button>
-                <x-section-button class="">
-                    {{ __('TF5') }}
-                </x-section-button>
-                <x-section-button class="">
-                    {{ __('TPB') }}
-                </x-section-button>
-                <a href="{{ url('/postulado') }}">
-                    <x-section-button class="">
-                        {{ __('Datos Postulantes') }}
-                    </x-section-button>
-                </a>
-            </div>
-        </div>
+
         <x-atoms.searchbar></x-atoms.searchbar>
     </div>
     @include('/components/tables/bootcamp-table')
