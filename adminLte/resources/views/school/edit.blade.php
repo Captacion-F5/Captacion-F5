7773d2123419442bb8adba93b9339ebd6b45f300
Mónica R <a20monicarr@gmail.com>
2023-04-23 14:04:32 +0200

    delete button index

--- adminLte/resources/views/school/edit.blade.php
+++ adminLte/resources/views/school/edit.blade.php
@@ -19,11 +19,7 @@
                         <span class="invalid-feedback">{{ $message }}</span>
                     @enderror
                 </div>
-
             <div class="flex justify-around mt-5">
-                <a class="text-lg" href="{{ route('school.index') }}">
-                    <i class="fa fa-school text-naranja m-2 "></i>Todas las escuelas
-                </a>
                 <button type="submit" class="bg-green-600 hover:bg-green-400 text-white font-bold py-2 px-4 rounded">
                     Actualizar escuela
                 </button>
